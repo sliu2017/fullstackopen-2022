33dd55474c3a297115e3f010b23bc08d7f4b7b9f
sliu2017 <liu.sonnys@gmail.com>
2022-12-29 02:44:50 -0500

    Completed courseinfo exercises

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,58 @@
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part part={part} />
+      ))}
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts
+    .map((part) => part.exercises)
+    .reduce((exercises, currentValue) => exercises + currentValue, 0);
+  return <p>Number of exercises {totalExercises}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
